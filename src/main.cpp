703d0dab549f5c214012b62b600559035e60ca9e
Chris <52449218+shadow578@users.noreply.github.com>
2024-10-17 07:40:59 +0000

    implement wait_for using WDT + minor reset refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,77 @@
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 #include <avr/power.h>
+#include <avr/sleep.h>
 #include <avr/wdt.h>
-#include <util/delay.h>
 
 // pin definitions
 constexpr uint8_t PIN_PWR_DOWN = PB0;
 constexpr uint8_t PIN_LOAD_EN = PB1;
 
 // for how long the device should sleep before turning on the load again
-constexpr uint32_t SLEEP_TIME = 30 * 60; // seconds
+// uses the WDT for timing, so will be fairly inaccurate.
+constexpr uint32_t SLEEP_TIME = 30 * 60; // seconds, rounded up to nearest 8s
 
-void wait_for(const uint32_t ms)
+EMPTY_INTERRUPT(WDT_vect);
+
+/**
+ * Wait for the specified number of seconds.
+ * 
+ * @param seconds the number of seconds to wait for
+ * 
+ * @note 
+ * this function doesn't disable any peripherals. if desired, disable them before calling this function.
+ * 
+ * @note
+ * this function alters the watchdog timer configuration.
+ * additionally, after this function returns, the watchdog timer is disabled.
+ * 
+ * @note
+ * after this function returns, interrupts are disabled.
+ * 
+ * @note
+ * loosely based on https://electronics.stackexchange.com/a/151743
+ */
+void wait_for(const uint32_t seconds)
 {
-    // _delay_ms() only works ok for values < 262.14 ms, so we call it multiple times in a loop
-    constexpr uint32_t SLEEP_TIME_TICK_MS = 100;
+    cli();
 
-    const uint32_t ticks = (ms * 1000UL) / SLEEP_TIME_TICK_MS;
-    for (uint32_t i = 0; i < ticks; i++)
+    // enable the watchdog timer with a timeout of 8s, in interrupt mode
+    wdt_reset();
+    MCUSR &= ~_BV(WDRF);          // clear wdt reset flag
+    WDTCR = _BV(WDE) | _BV(WDCE); // enable WDT
+    WDTCR = _BV(WDE) | _BV(WDTIE) | WDTO_8S; // enable WDT interrupt, set timeout to 8s
+
+    // sleep for the specified time
+    for (uint32_t i = 0; i < seconds; i += 8)
     {
-        _delay_ms(SLEEP_TIME_TICK_MS);
+        cli();                               // disable interrupts
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN); // only WDT can wake up the MCU
+        wdt_reset();                         // start wdt at zero
+        sei();                               // enable interrupts
+        sleep_mode();                        // sleep until WDT interrupt
+        sleep_disable();
     }
+
+    cli();
+
+    // disable WDT
+    wdt_reset();
+    wdt_disable();
+}
+
+/**
+ * Reset the CPU.
+ */
+void reset_cpu()
+{
+    // to reset the cpu, we enable the watchdog timer with a very short timeout.
+    // then, we hang in a loop until the watchdog timer resets the CPU for us.
+
+    // note: wdt_enable resets WDTIE, so we don't need to disable it explicitly
+    wdt_enable(WDTO_15MS);
+    while (1)
+        ;
 }
 
 int main()
@@ -29,20 +80,20 @@ int main()
     cli();
 
     // disable ADC, internal COMP, and WDT to reduce power consumption
-    ADCSRA &= ~_BV(ADEN);   // ADC enable = 0
-    power_adc_disable();    // ADC power reduction (PRR.PRADC) = 1
     ADCSRB &= ~_BV(ACME);   // analog comparator multiplexer enable = 0
     ACSR |= _BV(ACD);       // analog comparator disable = 1
-    WDTCR &= ~_BV(WDE);     // disable watchdog timer
-    wdt_disable();          // "
+    ADCSRA &= ~_BV(ADEN);   // ADC enable = 0
+    power_adc_disable();    // ADC power reduction (PRR.PRADC) = 1
+    wdt_disable();          // disable watchdog timer
     power_timer0_disable(); // timer0 power reduction (PRR.PRTIM0) = 1
+    // power_all_disable();
 
     // ensure all pins start out as input with no pull-up
-    DDRB = 0x00;
-    PORTB = 0x00;
+    DDRB = 0x00;  // set all pins to input
+    PORTB = 0x00; // disable pull-up on all pins
 
     // set power down pin to input
-    // DDRB &= ~_BV(PIN_PWR_DOWN);
+    // DDRB &= ~_BV(PIN_PWR_DOWN); // redundant: all pins are input at this point
 
     // set LOAD_EN to output, set HIGH
     DDRB |= _BV(PIN_LOAD_EN);
@@ -53,22 +104,16 @@ int main()
         ;
 
     // set all pins to input, no pull-up, to reduce power consumption
-    DDRB = 0x00;  // set all pins to input
-    PORTB = 0x00; // disable pull-up on all pins
-
-    // TODO: more power consumption reduction possible ?
-    // note: cannot enter any of the sleep mode, since they all disable the CPU.
+    DDRB = 0x00;
+    PORTB = 0x00;
 
     // wait for ~30 minutes
     wait_for(SLEEP_TIME);
 
-    // reset the MCU using watchdog timer
-    // for this, enable the watchdog timer with a short timeout and then hang until it resets the MCU.
-    // since the WDTs oscillator is not very accurate, the actual timeout may vary.
-    // using 250ms should fall somewhere between 100ms and 750ms.
-    wdt_enable(WDTO_250MS);
-    while (1)
-        ;
-
+    // reset the CPU
+    reset_cpu();
     return 0;
 }
+
+
+void setup();
