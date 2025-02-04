6fa97b600b0faa844b27dd563bd877a430990912
Chris <52449218+shadow578@users.noreply.github.com>
2025-02-04 13:43:10 +0000

    implement i2c control

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@
 
 
 // pin definitions
-constexpr uint8_t PIN_PWR_DOWN = PB2; // pin to indicate that the load should be powered down
 constexpr uint8_t PIN_LOAD_EN = PB1;  // pin attached to load mosfet gate
 
-// PIN_PWR_DOWN state when asserted (= should power down the load)
-constexpr bool PWR_DOWN_ASSERTED_STATE = false; // pulled high; active low
+// i2c constants
+constexpr uint8_t I2C_DEVICE_ADDRESS = 0x64;
+constexpr uint8_t I2C_COMMAND_SLEEP = 0x01;
+constexpr uint8_t I2C_COMMAND_GET_COUNT = 0x02;
 
 // for how long the device should sleep before turning on the load again
 // uses the WDT for timing, so will be fairly inaccurate even with correction factor applied.
@@ -22,6 +23,28 @@ constexpr uint32_t SLEEP_TIME = (30 * 60); // 30 minutes
 // correction factor for sleep time.
 // calibrate by measuring actual sleep time, then set this to (desired sleep time / actual sleep time)
 constexpr double SLEEP_TIME_CORRECTION = (30.0 / 37.0);
+
+static bool go_sleep = false;
+
+void on_i2c_request()
+{
+    const uint8_t command = i2c_device::read();
+    switch (command)
+    {
+    case I2C_COMMAND_SLEEP:
+        i2c_device::write(0x00);
+        break;
+    case I2C_COMMAND_GET_COUNT:
+        // TODO: actually send meaningful data
+        i2c_device::write(0xde);
+        i2c_device::write(0xad);
+        i2c_device::write(0xbe);
+        i2c_device::write(0xef);
+        break;
+    default:
+        break;
+    }
+}
 
 int main()
 {
@@ -34,28 +57,20 @@ int main()
     // ensure all pins start out as input with no pull-up
     lp::reset_gpio();
 
-    // set power down pin to input
-    // DDRB &= ~_BV(PIN_PWR_DOWN); // redundant: all pins are input at this point
+    // setup i2c device
+    i2c_device::begin(I2C_DEVICE_ADDRESS);
+    i2c_device::on_request(on_i2c_request);
 
-    // set LOAD_EN to output, set HIGH
+    // set LOAD_EN to output, set HIGH to turn on load
     DDRB |= _BV(PIN_LOAD_EN);
     PORTB |= _BV(PIN_LOAD_EN);
 
-    // wait until power down pin is asserted
-    if (PWR_DOWN_ASSERTED_STATE)
-    {
-        // active high, wait until high
-        while (!(PINB & _BV(PIN_PWR_DOWN)))
-            _delay_ms(10);
-    }
-    else
-    {
-        // active low, wait until low
-        while (PINB & _BV(PIN_PWR_DOWN))
-            _delay_ms(10);
-    }
+    // run idle while not sleeping
+    while(!go_sleep)
+        _delay_ms(10);
 
     // reset all pins for low-power
+    // this also turns off the load
     lp::reset_gpio();
 
     // wait for ~30 minutes
